bf9e24c007edca92f0bf3333dfd2a8d32a464728
huseyinsimsekk <hsynsimsek723@gmail.com>
2020-10-08 17:28:42 +0300

    Grouping clause edited

--- Linqer/Clauses/GroupingClauses.cs
+++ Linqer/Clauses/GroupingClauses.cs
@@ -16,7 +16,39 @@ namespace Linqer.Clauses
         public void GroupByInQuerySyntax()
         {
             var groups = from em in provider.GenerateEmployee()
-                         group em by em.EmployeeTypeId;
+                         group em by em.EmployeeTypeId; // Groping by EmployeeTypeId
+            foreach (var group in groups)
+            {
+                var counter = 0;
+                Console.WriteLine("Group Key: {0}", group.Key);
+                foreach (var employee in group)
+                {
+                    Console.WriteLine("{0}. Employee Name: {1}, Salary: {2}", counter + 1, employee.Name, employee.Salary);
+                }
+                Console.WriteLine("*********************");
+            }
+        }
+        public void GroupByInMethodSyntax()
+        {
+            var groups = provider.GenerateEmployee().GroupBy(m => m.EmployeeTypeId); // Groping by EmployeeTypeId
+
+            foreach (var group in groups)
+            {
+                var counter = 0;
+                Console.WriteLine("Group Key: {0}", group.Key);
+                foreach (var employee in group)
+                {
+                    Console.WriteLine("{0}. Employee Name: {1}, Salary: {2}", counter + 1, employee.Name, employee.Salary);
+                }
+                Console.WriteLine("*********************");
+            }
+        }
+
+        // ToLookup is the same as GroupBy; the only difference is GroupBy execution is deferred, whereas ToLookup execution is immediate.
+        public void ToLookupInMethodSyntax()
+        {
+            var groups = provider.GenerateEmployee().ToLookup(m => m.EmployeeTypeId); // Groping by EmployeeTypeId
+
             foreach (var group in groups)
             {
                 var counter = 0;
