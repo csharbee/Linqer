cc916b1fbf84910832eec4b1a2e5e3bc71e75039
huseyinsimsekk <hsynsimsek723@gmail.com>
2020-10-28 13:03:00 +0300

    ConcatMethod added

--- Linqer/Clauses/ConcatinationClause.cs
+++ Linqer/Clauses/ConcatinationClause.cs
@@ -1,10 +1,29 @@
-﻿using System;
+﻿using Linqer.Data;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Linqer.Clauses
 {
     public class ConcatinationClause
     {
+        private readonly List<Employee> Employees;
+        public ConcatinationClause()
+        {
+            var provider = new DataProvider();
+            Employees = provider.GenerateEmployee();
+        }
+        public void ConcatMethod()
+        {
+            var newEmp = new List<Employee>() {
+                                        new Employee() { Id = 124, EmployeeTypeId = 1, Name = "Merry", LastName = "Born", Salary = 3000 },
+                                        new Employee() { Id = 125, EmployeeTypeId = 2, Name = "John", LastName = "Sweat", Salary = 4500} };
+            var allEmployee = Employees.Concat(newEmp);
+            foreach (var employee in allEmployee)
+            {
+                Console.WriteLine($"Employee Name: {employee.Name}");
+            }
+        }
     }
 }
--- Linqer/Program.cs
+++ Linqer/Program.cs
@@ -10,46 +10,46 @@ namespace Linqer
             // Tutorial Resource : https://www.tutorialsteacher.com/linq/linq-tutorials 
 
             // Filtering Methods
-            //var filterClause = new FilteringClauses();
-            //filterClause.WhereFirstOverloadInQuerySyntax();    // 1- Where filter in query syntax  
-            //filterClause.WhereFirstOverloadInMethodSyntax();   // 2- Where filter in method (lambda) syntax
-            //filterClause.WhereSecondOverloadInMethodSyntax();  // 3- Another overload where filter method (using index) in method syntax
+            var filterClause = new FilteringClauses();
+            filterClause.WhereFirstOverloadInQuerySyntax();    // 1- Where filter in query syntax  
+            filterClause.WhereFirstOverloadInMethodSyntax();   // 2- Where filter in method (lambda) syntax
+            filterClause.WhereSecondOverloadInMethodSyntax();  // 3- Another overload where filter method (using index) in method syntax
 
-            //filterClause.OfTypeMethodInQuerySyntax();         // 4- OfType is a filter method which filter specific type in mixed type collection 
+            filterClause.OfTypeMethodInQuerySyntax();         // 4- OfType is a filter method which filter specific type in mixed type collection 
 
-            ////// Sorting Methods
-            //var sortingClause = new SortingClauses();
-            //sortingClause.OrderByInQuerySynax();              // 5-  Order By collection in query syntax
-            //sortingClause.OrderByInMethodSynax();             // 6-  Order By collection in method syntax
-            //sortingClause.OrderByDescendingInQuerySynax();    // 7-  Order By Descending collection in query syntax
-            //sortingClause.OrderByDescendingInMethodSynax();   // 8-  Order By Descending collection in method syntax
-            //sortingClause.ThenByInMethodSynax();              // 9-  Multiple ordering collection in method syntax
-            //sortingClause.ThenByDescendingInMethodSynax();    // 10- ThenByDescending method allows you to multiple descending ordering collection
+            //// Sorting Methods
+            var sortingClause = new SortingClauses();
+            sortingClause.OrderByInQuerySynax();              // 5-  Order By collection in query syntax
+            sortingClause.OrderByInMethodSynax();             // 6-  Order By collection in method syntax
+            sortingClause.OrderByDescendingInQuerySynax();    // 7-  Order By Descending collection in query syntax
+            sortingClause.OrderByDescendingInMethodSynax();   // 8-  Order By Descending collection in method syntax
+            sortingClause.ThenByInMethodSynax();              // 9-  Multiple ordering collection in method syntax
+            sortingClause.ThenByDescendingInMethodSynax();    // 10- ThenByDescending method allows you to multiple descending ordering collection
 
-            ////// Grouping Methods
-            //var groupByClauses = new GroupingClauses();
-            //groupByClauses.GroupByInQuerySyntax();            // 11- Group By collection according to a property, Then returns grouping collection
-            //groupByClauses.GroupByInMethodSyntax();           // 12- Similar to 11. Using method syntax
-            //groupByClauses.ToLookupInMethodSyntax();          // 13- ToLookup is the same as GroupBy; the only difference is GroupBy execution is deferred, whereas ToLookup execution is immediate.
+            //// Grouping Methods
+            var groupByClauses = new GroupingClauses();
+            groupByClauses.GroupByInQuerySyntax();            // 11- Group By collection according to a property, Then returns grouping collection
+            groupByClauses.GroupByInMethodSyntax();           // 12- Similar to 11. Using method syntax
+            groupByClauses.ToLookupInMethodSyntax();          // 13- ToLookup is the same as GroupBy; the only difference is GroupBy execution is deferred, whereas ToLookup execution is immediate.
 
-            ////// Joining Methods
-            //var joiningClause = new JoiningClauses();
-            //joiningClause.JoinInQuerySyntax();               // 14- Join two collections according to same property and select new joined collection by query syntax
-            //joiningClause.JoinInMethodSyntax();              // 15- Join two collections according to same property and select new joined collection by method syntax
+            //// Joining Methods
+            var joiningClause = new JoiningClauses();
+            joiningClause.JoinInQuerySyntax();               // 14- Join two collections according to same property and select new joined collection by query syntax
+            joiningClause.JoinInMethodSyntax();              // 15- Join two collections according to same property and select new joined collection by method syntax
 
-            ////// Selection Methods
-            //var selectionClause = new SelectionClauses();
-            //selectionClause.SelectAnonymousTypeInQuerySyntax();   // 16- Select new anonymous type collection in following collection in query syntax
-            //selectionClause.SelectAnonymousTypeInMethodSyntax();  // 17- Select new anonymous type collection in following collection in method syntax
-            //selectionClause.SelectCollectionTypeInMethodSyntax(); // 18- Select some value or some same collection in following collection in method syntax
+            //// Selection Methods
+            var selectionClause = new SelectionClauses();
+            selectionClause.SelectAnonymousTypeInQuerySyntax();   // 16- Select new anonymous type collection in following collection in query syntax
+            selectionClause.SelectAnonymousTypeInMethodSyntax();  // 17- Select new anonymous type collection in following collection in method syntax
+            selectionClause.SelectCollectionTypeInMethodSyntax(); // 18- Select some value or some same collection in following collection in method syntax
 
-            ////// Quentifier Methods
-            //var quentifierClause = new QuantifierClauses();
-            //quentifierClause.AllMethod();                       // 19- Checks if all the elements in a sequence satisfies the specified condition 
-            //quentifierClause.ContainsMethod();                  // 20- Checks if the sequence contains a specific element
-            //quentifierClause.AnyMethod();                       // 21- Checks if any of the elements in a sequence satisfies the specified condition
+            //// Quentifier Methods
+            var quentifierClause = new QuantifierClauses();
+            quentifierClause.AllMethod();                       // 19- Checks if all the elements in a sequence satisfies the specified condition 
+            quentifierClause.ContainsMethod();                  // 20- Checks if the sequence contains a specific element
+            quentifierClause.AnyMethod();                       // 21- Checks if any of the elements in a sequence satisfies the specified condition
 
-            //// Aggregate Methods
+            // Aggregate Methods
             var aggreageClause = new AggregationClauses();
             aggreageClause.AggregateMethod();                   // 22- Performs a custom aggregation operation on the values in the collection. 
             aggreageClause.CountMethod();                       // 23- Counts the elements in a collection.
@@ -75,6 +75,9 @@ namespace Linqer
             equalityClause.SequenceEqual();
             equalityClause.SequenceEqualForReferenceTypes();
 
+            // Concatination Method
+            var concatClause = new ConcatinationClause();
+            concatClause.ConcatMethod();
         }
     }
 }
