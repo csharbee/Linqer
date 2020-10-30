61336f6b0a83532c08a7e8f041a17df08a862437
huseyinsimsekk <hsynsimsek723@gmail.com>
2020-10-30 09:01:54 +0300

    defaultIfEmpty method added

---
+++ Linqer/Clauses/Misc.cs
@@ -0,0 +1,18 @@
+﻿using Linqer.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Linqer.Clauses
+{
+    public class Misc
+    {
+        public void DefaultIfEmptyMethod()
+        {
+            var emp = new List<Employee>();
+            var listOfEmployee = emp.DefaultIfEmpty();
+            Console.WriteLine($"Employee List Count: {listOfEmployee.Count()}");
+        }
+    }
+}
